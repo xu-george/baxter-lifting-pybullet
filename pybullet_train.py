e2e5fed5f8b26a87009f4e54dce2d2d66c8a7849
xu-geroge <binzhaoxu@gmail.com>
2022-04-13 09:51:50 +0400

    only position based grasp

--- pybullet_train.py
+++ pybullet_train.py
@@ -16,7 +16,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
     # --------------------environment ---------------------
     parser.add_argument('--env-name', default='BaxterPaddleGrasp_auto_gamma_0.99')
-    parser.add_argument('--epoch_step', default=200)
+    parser.add_argument('--epoch_step', default=100)
 
     # -------------------sac agent -------------------------
     parser.add_argument('--end_epoch', type=int, default=1e4)
@@ -24,13 +24,13 @@ def parse_args():
                         help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
     parser.add_argument('--eval', type=bool, default=True,
                         help='Evaluates a policy a policy every 10 episode (default: True)')
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+    parser.add_argument('--gamma', type=float, default=0.985, metavar='G',
                         help='discount factor for reward (default: 0.985)')
-    parser.add_argument('--tau', type=float, default=0.01, metavar='G',
-                        help='target smoothing coefficient(τ) (default: 0.03)')
+    parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                        help='target smoothing coefficient(τ) (default: 0.005)')
     parser.add_argument('--lr', type=float, default=1e-3, metavar='G',
                         help='learning rate (default: 1e-3)')
-    parser.add_argument('--alpha', type=float, default=0.3, metavar='G',
+    parser.add_argument('--alpha', type=float, default=0.5, metavar='G',
                         help='Temperature parameter α determines the relative importance of the entropy\
                             term against the reward (default: 0.3)')
     parser.add_argument('--alpha_lr', type=float, default=1e-4, metavar='G',
@@ -43,12 +43,12 @@ def parse_args():
                         help='batch size (default: 256)')
     parser.add_argument('--num_steps', type=int, default=1e8, metavar='N',
                         help='maximum number of steps (default: 1000000)')
-    parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+    parser.add_argument('--hidden_size', type=int, default=512, metavar='N',
                         help='hidden size (default: 1024)')
     parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
                         help='model updates per simulator step (default: 1)')
-    parser.add_argument('--start_steps', type=int, default=2000, metavar='N',
-                        help='Steps sampling random actions (default: 2000)')
+    parser.add_argument('--start_steps', type=int, default=3000, metavar='N',
+                        help='Steps sampling random actions (default: 3000)')
     parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
                         help='Value target update per no. of updates per step (default: 1)')
     parser.add_argument('--replay_size', type=int, default=1e6, metavar='N',
